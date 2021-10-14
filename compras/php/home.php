24f2af4f4ce895afa7d47ea28ae8b2dbbb51ad58
MeryCDC <desarrollo2@controldecarga.com>
2021-10-14 10:36:48 -0500

    If para evitar errores

--- compras/php/home.php
+++ compras/php/home.php
@@ -13,25 +13,28 @@
         $obj = $result->fetch_object();
         $confirmar_datos_usuario = $obj->confirmar_datos;
         $id_usuario = $obj->id;
-
+ 
         //Información General de Cliente
         $resultado = $conexion->query(" select * from clientes where id_usuario='$id_usuario' ");
         $obj_cliente = $resultado->fetch_object();
-        $nombres_obj_cliente = $obj_cliente->nombres;
-        $apellidos_obj_cliente = $obj_cliente->apellidos;
-        $nombre_completo = $nombres_obj_cliente . " " . $apellidos_obj_cliente;
 
-        $calle_obj_cliente = $obj_cliente->calle;
-        $numero_ext_obj_cliente = $obj_cliente->numero_exterior;
-        $numero_int_obj_cliente = $obj_cliente->numero_interior;
-        $entre_calles_obj_cliente = $obj_cliente->entre_calles;
-        $colonia_obj_cliente = $obj_cliente->colonia;
-        $codigo_postal_obj_cliente = $obj_cliente->codigo_postal;
-        $estado_obj_cliente = $obj_cliente->estado;
-        $ciudad_obj_cliente = $obj_cliente->ciudad;
-        $referencias_obj_cliente = $obj_cliente->referencias;
-        $numero_telefono_obj_cliente = $obj_cliente->telefono;
-        $numero_aux_obj_cliente = $obj_cliente->telefono_auxiliar;
+        if($obj_cliente > 0){
+            $nombres_obj_cliente = $obj_cliente->nombres;
+            $apellidos_obj_cliente = $obj_cliente->apellidos;
+            $nombre_completo = $nombres_obj_cliente . " " . $apellidos_obj_cliente;
+    
+            $calle_obj_cliente = $obj_cliente->calle;
+            $numero_ext_obj_cliente = $obj_cliente->numero_exterior;
+            $numero_int_obj_cliente = $obj_cliente->numero_interior;
+            $entre_calles_obj_cliente = $obj_cliente->entre_calles;
+            $colonia_obj_cliente = $obj_cliente->colonia;
+            $codigo_postal_obj_cliente = $obj_cliente->codigo_postal;
+            $estado_obj_cliente = $obj_cliente->estado;
+            $ciudad_obj_cliente = $obj_cliente->ciudad;
+            $referencias_obj_cliente = $obj_cliente->referencias;
+            $numero_telefono_obj_cliente = $obj_cliente->telefono;
+            $numero_aux_obj_cliente = $obj_cliente->telefono_auxiliar;
+        }
         echo $confirmar_datos_usuario;
 /*         echo 
         '<!DOCTYPE html>
